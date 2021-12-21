0c8beed6e731406bb78fbd75880f8aabadab5bed
Jessie Lesbian <jessietessie256@gmail.com>
2021-12-21 13:56:12 +0700

    Major MyEUBIWallet upgrade

--- scripts.js
+++ scripts.js
@@ -21,54 +21,17 @@
 		});
 	};
 	
-	//Blockchain Manager
+	//Blockchain Managers
 	const BlockchainManager = new Web3.modules.Eth('https://speedy-nodes-nyc.moralis.io/41590f438df3f8018a1e84b1/bsc/mainnet');
+	const PolyManager = new Web3.modules.Eth('https://speedy-nodes-nyc.moralis.io/41590f438df3f8018a1e84b1/polygon/mainnet');
 	
 	//Token managers
 	let erc20 = [{"inputs": [{"internalType": "address","name": "owner","type": "address"},{"internalType": "address","name": "spender","type": "address"}],"name": "allowance","outputs": [{"internalType": "uint256","name": "","type": "uint256"}],"stateMutability": "view","type": "function"},{"inputs": [{"internalType": "address","name": "spender","type": "address"},{"internalType": "uint256","name": "amount","type": "uint256"}],"name": "approve","outputs": [{"internalType": "bool","name": "","type": "bool"}],"stateMutability": "nonpayable","type": "function"},{"inputs": [{"internalType": "address","name": "account","type": "address"}],"name": "balanceOf","outputs": [{"internalType": "uint256","name": "","type": "uint256"}],"stateMutability": "view","type": "function"},{"inputs": [{"internalType": "address","name": "recipient","type": "address"},{"internalType": "uint256","name": "amount","type": "uint256"}],"name": "transfer","outputs": [{"internalType": "bool","name": "","type": "bool"}],"stateMutability": "nonpayable","type": "function"},{"inputs": [{"internalType": "address","name": "sender","type": "address"},{"internalType": "address","name": "recipient","type": "address"},{"internalType": "uint256","name": "amount","type": "uint256"}],"name": "transferFrom","outputs": [{"internalType": "bool","name": "","type": "bool"}],"stateMutability": "nonpayable","type": "function"}];
 	const bEUBI = new BlockchainManager.Contract(erc20, '0x27faaa5bd713dcd4258d5c49258fbef45314ae5d');
 	const bPRSS = new BlockchainManager.Contract(erc20, '0x17e6d3E7B727b31Ab6eB9B5b0A38f00389589c80');
+	const PolyEUBI = new PolyManager.Contract(erc20, '0x553e77f7f71616382b1545d4457e2c1ee255fa7a');
 	//const bEUBI_LP = new BlockchainManager.Contract(erc20, '0x7700eea0633210437ffcc6b10c02e0515054b08f');
 	erc20 = undefined;
-	
-	//EXPERIMENTAL: TRANSACTION HISTORY VIEWER
-	let TXHistory = [];
-	let TXHistoryOffset = 0;
-	const reloadTransactionHistory = async function(shift){
-		let temphtml = '';
-		TXHistoryOffset += shift;
-		if(TXHistoryOffset < 0){
-			TXHistoryOffset = 0;
-		}
-		let c = 0;
-		for(let i = TXHistoryOffset; i < TXHistory.length; i++){
-			const current = TXHistory[i];
-			if(current.gasUsed == '21000'){
-				if(current.from == address){
-					if(current.to != address){
-						temphtml += '<p>You => ' + escapeHtml(current.to) + ' ' + Web3.utils.fromWei(current.value) + ' BNB @ ' + new Date(parseInt(current.timeStamp) * 1000).toString() + '</p>';
-					}
-				} else {
-					temphtml += '<p>' + escapeHtml(current.to) + ' => You' + ' ' + Web3.utils.fromWei(current.value) + ' BNB @ ' + new Date(parseInt(current.timeStamp) * 1000).toString() + '</p>';
-				}
-				if(c++ == 9){
-					break;
-				}
-			}
-		}
-		if(c == 0){
-			TXHistoryOffset -= shift;
-		} else{
-			transactionHistory.innerHTML = temphtml;
-		}
-		
-	};
-	previousTransactionHistoryButton.onclick = function(){
-		reloadTransactionHistory(-10);
-	};
-	nextTransactionHistoryButton.onclick = function(){
-		reloadTransactionHistory(10);
-	};
 	
 	//Account Manager
 	let address = undefined;
@@ -77,9 +40,11 @@
 		eubiBalance.innerHTML = 'Fetching bEUBI balance...';
 		prssBalance.innerHTML = 'Fetching bPRSS balance...';
 		nativeBalance.innerHTML = 'Fetching BNB balance...';
+		polyeubiBalance.innerHTML = 'Fetching PolyEUBI balance...';
+		maticBalance.innerHTML = 'Fetching MATIC balance...';
 		
-		//Batch load balances
-		const batch = new BlockchainManager.BatchRequest();
+		//Batch load balances for Binance Smart Chain
+		let batch = new BlockchainManager.BatchRequest();
 		const updateTokenBalance = function(target, contract, name){
 			batch.add(contract.methods.balanceOf(address).call.request(async function(fail, pass){
 				if(pass){
@@ -102,25 +67,17 @@
 		updateTokenBalance(prssBalance, bPRSS, 'bPRSS');
 		batch.execute();
 		
-		//EXPERIMENTAL: TRANSACTION HISTORY VIEWER
-		var xhttp = new XMLHttpRequest();
-		xhttp.onreadystatechange = async function() {
-			if (this.readyState == 4 && this.status == 200) {
-			    const res = JSON.parse(xhttp.responseText);
-			    if(res == undefined){
-					return;
-			    }
-			    if(res.result == undefined){
-					return;
-				}
-				TXHistory = res.result;
-				reloadTransactionHistory(0);
+		//Batch load balances for Polygon Sidechain
+		batch = new PolyManager.BatchRequest();
+		batch.add(PolyManager.getBalance.request(address, async function(fail, pass){
+			if(pass){
+				maticBalance.innerHTML = escapeHtml(Web3.utils.fromWei(pass)) + ' MATIC';
+			} else{
+				maticBalance.innerHTML = 'unable to fetch MATIC balance!';
 			}
-		};
-		xhttp.open("GET", "https://api.bscscan.com/api?module=account&action=txlist&startblock=0&sort=asc&address=" + address, true);
-		xhttp.send();
-		
-		
+		}));
+		updateTokenBalance(polyeubiBalance, PolyEUBI, 'PolyEUBI');
+		batch.execute();	
 	};
 	let tempSignAndSendTransaction = undefined;
 	{
@@ -140,6 +97,15 @@
 			beforeWalletLoad.style.display = 'block';
 			afterWalletLoad.style.display = 'none';
 		};
+		const exportJSON = async function(exportedJSON){
+			const element = document.createElement('a');
+			element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify(exportedJSON)));
+			element.setAttribute('download', 'keystore-' + escapeHtml(exportedJSON.address) + '.json');
+			element.style.display = 'none';
+			document.body.appendChild(element);
+			element.click();
+			document.body.removeChild(element);
+		}
 		createWalletButton.onclick = function(){
 			loadAccountManager(BlockchainManager.accounts.create());
 		};
@@ -152,6 +118,11 @@
 			} catch{
 				InvalidKeyModalInstance.open();
 			}
+		};
+		
+		//Export private key
+		copyPrivateButton.onclick = function(){
+			navigator.clipboard.writeText(AccountManager.privateKey);
 		};
 		
 		//Quick wallet access operations
@@ -193,6 +164,16 @@
 				localStorage.setItem('MyEUBIWallet_QuickWalletAccess', JSON.stringify(QuickWalletAccess));
 				reloadQuickWalletAccess();
 				WalletSavedModalInstance.open();
+			};
+			
+			//Quick wallet access - export wallet as keystore
+			QuickDownloadQuickWalletAccess.onclick =  async function(){
+				const plainpassword2 = pass1.value;
+				if(plainpassword2 != pass2.value){
+					MismatchingPasswordModalInstance.open();
+					return;
+				}
+				exportJSON(AccountManager.encrypt(plainpassword2));
 			};
 			
 			//Quick wallet access - load wallet
@@ -249,14 +230,7 @@
 						const exportingWallet = {};
 						Object.assign(exportingWallet, QuickWalletAccess[i]);
 						exportingWallet.name = undefined;
-						const element = document.createElement('a');
-						element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify(exportingWallet)));
-						element.setAttribute('download', 'keystore.json');
-						element.style.display = 'none';
-						document.body.appendChild(element);
-						element.click();
-						document.body.removeChild(element);
-						console.log(QuickWalletAccess[i]);
+						exportJSON(exportingWallet);
 					}
 				}
 			};
@@ -266,23 +240,32 @@
 		}
 		
 		//Last function of the day
-		tempSignAndSendTransaction = async function(transaction){
-			transaction.chainId = '56';
+		tempSignAndSendTransaction = async function(transaction, isPolygon = false){
+			const SelectedBlockchainManager = isPolygon ? PolyManager : BlockchainManager;
+			if(isPolygon){
+				//Moralis bug workaround
+				transaction.chainId = '0x89';
+			} else{
+				transaction.chainId = '56';
+			}
 			transaction.from = address;
-			transaction;
 			let ix = 0;
-			const batch = new BlockchainManager.BatchRequest();
+			const batch = new SelectedBlockchainManager.BatchRequest();
 			const quicksend = function(){
 				if(ix++ == 2){
 					transaction.from = undefined;
 					//Despite this being "unfailable", we still need failure checks
 					//Since it's a cybersecurity best practice
+					if(isPolygon){
+						//Web3.js bug workaround
+						transaction.chainId = '137';
+					}
 					AccountManager.signTransaction(transaction).then(function(value){
-						BlockchainManager.sendSignedTransaction(value.rawTransaction).then(function(pass){
+						SelectedBlockchainManager.sendSignedTransaction(value.rawTransaction).then(function(pass){
 							SentModalInstance.open();
 							reloadWallet();
 						}, function(){
-							SendFailModal.open();
+							SendFailModalInstance.open();
 							reloadWallet();
 						});
 					}, function(fail){
@@ -290,7 +273,7 @@
 					});
 				}
 			};
-			batch.add(BlockchainManager.estimateGas.request(transaction, async function(fail, pass){
+			batch.add(SelectedBlockchainManager.estimateGas.request(transaction, async function(fail, pass){
 				if(pass){
 					transaction.gas = pass.toString();
 				} else{
@@ -298,7 +281,7 @@
 				}
 				quicksend();
 			}));
-			batch.add(BlockchainManager.getGasPrice.request(async function(fail, pass){
+			batch.add(SelectedBlockchainManager.getGasPrice.request(async function(fail, pass){
 				if(pass){
 					transaction.gasPrice = pass;
 					quicksend();
@@ -306,7 +289,7 @@
 					SignFailModalInstance.open();
 				}
 			}));
-			batch.add(BlockchainManager.getTransactionCount.request(address, async function(fail, pass){
+			batch.add(SelectedBlockchainManager.getTransactionCount.request(address, async function(fail, pass){
 				if(pass){
 					transaction.nonce = pass;
 					quicksend();
@@ -323,7 +306,7 @@
 		const signAndSendTransaction = tempSignAndSendTransaction;
 		//Cryptocurrency sending
 		sendNativeButton2.onclick = function(){
-			let transaction = {};
+			const transaction = {};
 			let value;
 			try{
 				value = Web3.utils.toWei(NativeAmount.value);
@@ -333,17 +316,20 @@
 			}
 			const adr2 = sendtoNative.value;
 			if(Web3.utils.isAddress(adr2)){
-				let contract_addr = sendingCryptoSelector.value;
-				if(contract_addr == 'bnb'){
-					//Sending BNB
-					transaction.to = adr2;
-					transaction.value = value;
-				} else{
-					//Sending tokens
-					transaction.to = contract_addr;
-					transaction.data = bEUBI.methods.transfer(adr2, value).encodeABI();
+				const contract_addr = sendingCryptoSelector.value;
+				switch(contract_addr){
+					case 'bnb':
+					case 'matic':
+						transaction.to = adr2;
+						transaction.value = value;
+						break;
+					default:
+						//Sending tokens
+						transaction.to = contract_addr;
+						transaction.data = bEUBI.methods.transfer(adr2, value).encodeABI();
+						break;
 				}
-				signAndSendTransaction(transaction);
+				signAndSendTransaction(transaction, contract_addr == 'matic' || contract_addr == '0x553e77f7f71616382b1545d4457e2c1ee255fa7a');
 			} else{
 				InvalidAddressModalInstance.open();
 			}
@@ -358,9 +344,9 @@
 	
 	//miscy
 	const toggle = async function (className, displayState){
-		var elements = document.getElementsByClassName(className)
+		const elements = document.getElementsByClassName(className)
 
-		for (var i = 0; i < elements.length; i++){
+		for (let i = 0; i < elements.length; i++){
 			elements[i].style.display = displayState;
 		}
 	}
